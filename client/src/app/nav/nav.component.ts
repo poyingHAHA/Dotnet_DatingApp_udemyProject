bda6407e08d349caebd849d29827b37576aa6e5b
poyingHAHA <poying.code@gmail.com>
2022-07-15 13:28:20 +0800

    section5 - Persisting the login by rxjs

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -13,6 +13,7 @@ export class NavComponent implements OnInit {
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login(){
@@ -30,7 +31,25 @@ export class NavComponent implements OnInit {
   }
 
   logout(){
+    this.accountService.logout();
     this.loggedIn = false;
+  }
+
+  observer = {
+    next: user => {
+      // double exclamation marks here. Turn our object into a boolean
+      // Now our user is either null or it's a user object.
+      // But if we use the double exclamation marks here, we effectively saying if the user is null,
+      // then this means it's false. And if the user is something, then then it works out to be true.
+      this.loggedIn = !!user;
+    },
+    error: error => {
+      console.log(error)
+    }
+  }
+
+  getCurrentUser(){
+    this.accountService.currentUser$.subscribe(this.observer);
   }
 
 }
