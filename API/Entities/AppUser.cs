15b4302ba62d477ba1a11e2c290265373afcceed
poyingHAHA <poying.code@gmail.com>
2022-07-17 13:51:33 +0800

    Section8 - Extending the user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -11,6 +11,14 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime CreatedBy { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
