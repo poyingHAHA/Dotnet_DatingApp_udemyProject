36a399d017706b5549e086f451c1703acac674c1
poyingHAHA <poying.code@gmail.com>
2022-07-17 14:34:18 +0800

    Section8 - 如何使用Entity Framework實作關聯表格

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+  // make the photo Entity to be called photos in the database
+  [Table("photos")] 
   public class Photo
   {
     public int Id { get; set; }
     public string Url { get; set; }
     public bool IsMain { get; set; }
     public string PublicId { get; set; }
+    public AppUser AppUser { get; set; }
+    public int AppUserId { get; set; }
   }
 }
