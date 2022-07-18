d14b5828158e928475f5316676a6fc1d1f82b98e
poyingHAHA <poying.code@gmail.com>
2022-07-18 09:30:58 +0800

    Section 9 - Creating Member Interface in TS

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,17 @@
+import { Photo } from "./Photo";
+
+export interface Root {
+  id: number;
+  username: string;
+  age: number;
+  knownAs: string;
+  photoUrl: string;
+  created: Date;
+  lastActive: Date;
+  gender: string;
+  introduction: string;
+  lookingFor: string;
+  interests: string;
+  country: string;
+  photos: Photo[];
+}
