fb4029dab3a9f9d1ee54f57abb2106e2aecba7b1
poyingHAHA <poying.code@gmail.com>
2022-07-18 12:47:59 +0800

    S9 - Routing to the detailed page

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,14 +1,8 @@
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
-  })
-}
 
 @Injectable({
   providedIn: 'root'
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member
 
-  constructor() { }
-
+  // So what we also need in here, because what we want to access, when a user clicks on any of these routes,
+  // then we're going to send up or they're going to use their username.
+  // Or we going to use the username to decide which user this is and we need to access that particular user's profile
+  // So what we need to do to get that is the activated route and what we'll say is private route and then we'll bring in activated route from Angular router.
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
+  
   ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe(member => {
+      this.member = member;
+    })
   }
 
 }
