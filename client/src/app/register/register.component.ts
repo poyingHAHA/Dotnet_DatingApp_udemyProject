9ee57bd3d930c401a4661496dc0d49bf1de8d5aa
poyingHAHA <poying.code@gmail.com>
2022-07-19 16:41:03 +0800

    S11 - Using the form builder service

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { ToastrService } from 'ngx-toastr';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -15,23 +15,19 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private accountService: AccountService,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
     this.initializeForm();
   }
 
   initializeForm(){
-    this.registerForm = new FormGroup({
-      // And inside each of these form controls, what we can do is we could give it a starting value, we could give it an initial value.
-      // And then what we can add, the next parameter is our validation options.
-      // And what we can specify here is validators and then we can choose from the out-of-box validators that Angular provides.
-      username: new FormControl('', Validators.required),
-      // And if we wanted to add two validators then we would put them inside an array.
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      // And then we'll need to add a custom validator so that we can compare the password field with the confirmpassword field.
-      //
-      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     })
     this.registerForm.controls.password.valueChanges.subscribe(() => {
       this.registerForm.controls.confirmPassword.updateValueAndValidity();
@@ -40,8 +36,6 @@ export class RegisterComponent implements OnInit {
 
   matchValues(matchTo: string): ValidatorFn{
     return (control: AbstractControl) => {
-      // What's what I'm just going to clarify now is that what we're doing here is we're getting access to the control that we're going to attach this validator to.
-      //   confirm password control.
       return control?.value === control?.parent?.controls[matchTo].value
         ? null : {isMatching: true}
     }
