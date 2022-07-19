42df7a1b4414c7ad0f414354fefc00709f5f30ea
poyingHAHA <poying.code@gmail.com>
2022-07-19 19:06:30 +0800

    S12- Update the API register method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Services;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,34 +18,36 @@ namespace API.Controllers
   {
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
+    private readonly IMapper _mapper;
 
-    public AccountController(DataContext context, ITokenService tokenService)
+    public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
     {
-        _context = context;
-        _tokenService = tokenService;
+      _context = context;
+      _tokenService = tokenService;
+      _mapper = mapper;
     }
 
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
-        if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+      if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
-        using var hmac = new HMACSHA512();
+      var user = _mapper.Map<AppUser>(registerDto);
 
-        var user = new AppUser
-        {
-            UserName = registerDto.Username.ToLower(),
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key
-        };
+      using var hmac = new HMACSHA512();
 
-        _context.Users.Add(user);
-        await _context.SaveChangesAsync();
+      user.UserName = registerDto.Username.ToLower();
+      user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+      user.PasswordSalt = hmac.Key;
 
-        return new UserDto
+      _context.Users.Add(user);
+      await _context.SaveChangesAsync();
+
+      return new UserDto
       {
         Username = user.UserName,
-        Token = _tokenService.CreateToken(user)
+        Token = _tokenService.CreateToken(user),
+        KnownAs = user.KnownAs
       };
     }
 
@@ -57,28 +60,29 @@ namespace API.Controllers
         x => x.UserName == loginDto.Username
       );
 
-      if(user == null) return Unauthorized("Invalid username");
+      if (user == null) return Unauthorized("Invalid username");
 
       using var hmac = new HMACSHA512(user.PasswordSalt);
 
       var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-      for(int i=0; i<computedHash.Length; i++)
+      for (int i = 0; i < computedHash.Length; i++)
       {
-        if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
       }
 
       return new UserDto
       {
         Username = user.UserName,
         Token = _tokenService.CreateToken(user),
-        PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+        PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+        KnownAs = user.KnownAs
       };
     }
 
     private async Task<bool> UserExists(string username)
     {
-        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+      return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
     }
 
   }
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -11,8 +11,19 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
         // And we're just adding an additional validator here so that we'll see additional responses when we hit this particular method.
-        [StringLength(8, MinimumLength = 4)] 
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
