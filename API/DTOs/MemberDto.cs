41603fe80c44e3ed5e3fbd5de9fc95725de6c93a
poyingHAHA <poying.code@gmail.com>
2022-07-20 12:44:45 +0800

    S13-Adding sorting functionality

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -15,7 +15,7 @@ namespace API.DTOs
         public int Age { get; set; }
         public string KnownAs { get; set; }
         public string PhotoUrl { get; set; }
-        public DateTime Created { get; set; }
+        public DateTime CreatedBy { get; set; }
         public DateTime LastActive { get; set; }
         public string Gender { get; set; }
         public string Introduction { get; set; }
--- API/Repositories/Impl/UserRepository.cs
+++ API/Repositories/Impl/UserRepository.cs
@@ -74,6 +74,12 @@ namespace API.Repositories.Impl
       var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
 
       query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+      query = userParams.OrderBy switch
+      {
+        "created" => query.OrderByDescending(u => u.CreatedBy),
+        // And the way that we specify default in this switch statement is an underscore  
+        _ => query.OrderByDescending(u => u.LastActive)
+      };
 
       return await PagedList<MemberDto>.CreateAsync(
         query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).AsNoTracking(),
