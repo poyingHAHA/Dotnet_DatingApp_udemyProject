f2c48870768cb297b22422d3e141c043d2c0dce8
poyingHAHA <poying.code@gmail.com>
2022-07-21 07:02:02 +0800

    S14-Adding the Like User

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -14,5 +14,27 @@ namespace API.Data
     }
 
     public DbSet<AppUser> Users { get; set; }
+
+    public DbSet<UserLike> Likes { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+
+      // we pass in the entity as a type parameter of what we want to configure.
+      builder.Entity<UserLike>()
+              .HasKey(k => new {k.SourceUserId, k.LikedUserId});// we're going to configure this key ourselves and it's going to be a combination of the source user ID and the liked user ID.
+
+      builder.Entity<UserLike>()
+            .HasOne(s => s.SourceUser)
+            .WithMany(l => l.LikedUsers)
+            .HasForeignKey(s => s.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+      builder.Entity<UserLike>()
+            .HasOne(s => s.LikedUser)
+            .WithMany(l => l.LikedByUsers)
+            .HasForeignKey(s => s.LikedUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
   }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -23,15 +23,9 @@ namespace API.Entities
         public string Country { get; set; }
         public string City { get; set; }
         public ICollection<Photo> Photos { get; set; }
-        
-        // In order for Mapper to go in, retrieve this, it's got to go and get the appUser.
-        // So we're kind of defeating the purpose of what we're doing here by doing it this way.
-        // So what we're going to do is not use this getAge methods to go and get the age.
-        // because it's not particularly efficient.
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
-        // But what we'll do is we'll go to our automatic profile and we'll add another property and say ForMember
+        // Who has like currently login user
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        // The currently user has liked
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
