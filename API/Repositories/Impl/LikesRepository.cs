aeb5f1d666b12a420accc771e0072372c1524a79
poyingHAHA <poying.code@gmail.com>
2022-07-21 08:37:46 +0800

    S14-Implementing the likes repository

--- API/Repositories/Impl/LikesRepository.cs
+++ API/Repositories/Impl/LikesRepository.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Repositories.Impl
@@ -17,19 +18,43 @@ namespace API.Repositories.Impl
         _context = context;
     }
 
-    public Task<UserLike> GetUserLike(int sourceId, int likedUserId)
+    public async Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
     {
-      throw new NotImplementedException();
+      return await _context.Likes.FindAsync(sourceUserId, likedUserId);
     }
 
-    public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+    public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
     {
-      throw new NotImplementedException();
+      var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
+      var likes = _context.Likes.AsQueryable();
+
+      if(predicate == "liked")
+      {
+        likes = likes.Where(like => like.SourceUserId == userId);
+        users = likes.Select(like => like.LikedUser);
+      }
+
+      if(predicate == "likedBy")
+      {
+        likes = likes.Where(like => like.SourceUserId == userId);
+        users = likes.Select(like => like.SourceUser);
+      }
+
+      return await users.Select(user => new LikeDto{
+        Username = user.UserName,
+        KnowAs = user.KnownAs,
+        Age = user.DateOfBirth.CalculateAge(),
+        PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain).Url,
+        City = user.City,
+        Id=user.Id
+      }).ToListAsync();
     }
 
-    public Task<AppUser> GetUserWithLikes(int userId)
+    public async Task<AppUser> GetUserWithLikes(int userId)
     {
-      throw new NotImplementedException();
+        return await _context.Users
+            .Include(x => x.LikedUsers)
+            .FirstOrDefaultAsync(x => x.Id == userId);
     }
   }
 }
